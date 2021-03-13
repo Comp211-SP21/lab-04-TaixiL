1de1c2bdd4cd3ca529ed6df1dcf8790b9fbd25cf
root <taixi@email.unc.edu>
2021-03-13 18:05:50 +0000

    d

--- bit_utils.c
+++ bit_utils.c
@@ -1,23 +1,14 @@
 // PID: 730401749
 // I pledge the COMP211 honor code.
 #include "bit_utils.h"
-#include <string.h>
 
-int power2 (int power) {
-    int power2 = 1;
-    for (int j=power; j>0; j--)
-        power2 = 2*power2;
-    return power2;
-}
-
-char* itob (int num, int size){
-
+char* itob (int num, int size) {
     char binary[size];
     binary[size] = 0;
     char zero = 48;
     char one = 49;
     
-    if (num>=0){
+    if (num>=0) {
         for (int i=0; i<size; i++)
             binary[i] = 48; 
     } else {
@@ -29,9 +20,11 @@ char* itob (int num, int size){
     }
     
     for (int i=size-1; i>0; i--) {
-        int power2Val = power2(i-1);
-        binary[size-i] = (power2Val<=num) ? one : zero;
-        num = (binary[size-i]==one) ? (num-power2Val) : num;
+        int powerVal = 1;
+        for (int j=i-1; j>0; j--)
+            powerVal = 2*powerVal;
+        binary[size-i] = (powerVal<=num) ? one : zero;
+        num = (binary[size-i]==one) ? (num-powerVal) : num;
     }
 
     char *str = (char*)malloc(sizeof(char)*(size));
@@ -39,27 +32,25 @@ char* itob (int num, int size){
     for (int i=0; i<size; i++) {
         str[i] = binary[i];
     }
-
     return str;
 }
 
-
-int mask_bits(int num, int bit){
+int mask_bits(int num, int bit) {
     int result = num & bit;
     return result;
 }
 
-int set_bits(int num, int bit){
+int set_bits(int num, int bit) {
     int result = num | bit;
     return result;
 }
 
-int inverse_bits(int num, int bit){
+int inverse_bits(int num, int bit) {
     int result = num ^ bit;
     return result;
 }
 
-int bit_select(int num, int mostSignificant, int leastSignificant){
+int bit_select(int num, int mostSignificant, int leastSignificant) {
     int number = mostSignificant - leastSignificant + 1;
     int result = (num >> (mostSignificant+1-number)) & ~(~0 << number);
     return result;
