4c22f12d7b0430e3367e720461ff83e76b8ef2e3
root <taixi@email.unc.edu>
2021-03-13 17:45:43 +0000

    f

--- bit_utils.c
+++ bit_utils.c
@@ -1,58 +1,47 @@
 // PID: 730401749
 // I pledge the COMP211 honor code.
 #include "bit_utils.h"
+#include <string.h>
 
-void negate_twos(char *bstring, int size) {
-    // reverse all 0s and 1s
-    for (int i=0; i < size; i++){
-        if (bstring[i] == '0')
-            bstring[i] = '1';
-        else
-            bstring[i] = '0';
-    }
-
-    // binary addition to add 1
-    for (int i=size-1; i >= 0; i--){
-        if (bstring[i] == '0'){
-            bstring[i] = '1';
-            return;
-        } else {
-            bstring[i] = '0';
-        }
-    }
+int power2 (int power) {
+    int power2 = 1;
+    for (int j=power; j>0; j--)
+        power2 = 2*power2;
+    return power2;
 }
 
-char* itob(int num, int size) {
+char* itob (int num, int size){
 
-    char output[size];
-    int i = 0;
-    int isnegative = 0;
+    char binary[size];
+    binary[size] = 0;
+    char zero = 48;
+    char one = 49;
+    
+    if (num>=0){
+        for (int i=0; i<size; i++)
+            binary[i] = 48; 
+    } else {
+        for (int i=0; i<size; i++)
+            binary[i] = 49;
+        zero = 49;
+        one = 48;
+        num = num - 2*num - 1;
+    }
+    
+    for (int i=size-1; i>0; i--) {
+        int power2Val = power2(i-1);
+        binary[size-i] = (power2Val<=num) ? one : zero;
+        num = (binary[size-i]==one) ? (num-power2Val) : num;
+    }
 
-        if (num < 0) 
-            isnegative = 1;
-
-        // assign string terminator
-        output[size] = '\0'; 
-
-        // Convert integer to binary string output
-        for (int i = size-1; i >= 0; i--) {
-            if (num % 2) {
-                output[i] = '1';
-            } else {
-                output[i] = '0';
-            }
-            num = num / 2;
-        }
-
-        if (isnegative) 
-            negate_twos(output, size);
-    char *str = (char*)malloc(sizeof(char)*(size)+1);
+    char *str = (char*)malloc(sizeof(char)*(size));
     str[size] = 0;
     for (int i=0; i<size; i++) {
-        str[i] = output[i];
+        str[i] = binary[i];
     }
+
     return str;
-    }
+}
 
 
 int mask_bits(int num, int bit){
